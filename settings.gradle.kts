f62ec1801d3fc8abe4b215ee1ee309acf7a5f294
Indra Mahkota <indra.mahkota@ajaib.co.id>
2022-08-27 12:29:30 +0700

    Edit some configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        gradlePluginPortal()
         maven { setUrl("https://jitpack.io") }
     }
 }
